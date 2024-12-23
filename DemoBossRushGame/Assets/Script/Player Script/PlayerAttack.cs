956414c0b82456769f38e888ade4da84c82be346
Stitch Punk <mrderpy0321@gmail.com>
2024-12-23 21:12:33 +0800

    ADD

--- DemoBossRushGame/Assets/Script/Player Script/PlayerAttack.cs
+++ DemoBossRushGame/Assets/Script/Player Script/PlayerAttack.cs
@@ -2,71 +2,104 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.InputSystem.DefaultInputActions;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public InputActionAsset inputActions; //connect it to the input system
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    public InputActionAsset inputActions;
+
     public float attackRange = 1.5f;
     public int damage = 5;
+    private bool isAttacking = false; // Prevent repeated attacks
 
     void Start()
     {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+
         var playerActions = inputActions.FindActionMap("Player");
-
         playerActions.FindAction("Attack").performed += OnAttack;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        playerActions.Enable();
     }
-
 
     private void OnAttack(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && !isAttacking)
         {
-            Debug.Log("Attack performed!");
+            // PerformAttack();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && !isAttacking)
+        {
             PerformAttack();
         }
     }
+
     private void PerformAttack()
     {
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, attackRange);
+        if (isAttacking) return;
 
-        Debug.Log("Enemies in range: " + hitEnemies.Length);
+        Debug.Log("Attack On!");
+        isAttacking = true; // Set flag to prevent repeated attacks
+
+        Vector2 lastMoveDirection = playerMovement.lastMoveDirection;
+
+        // Determine the attack animation based on movement direction
+        if (Mathf.Abs(lastMoveDirection.y) > Mathf.Abs(lastMoveDirection.x))
+        {
+            if (lastMoveDirection.y > 0f)
+            {
+                anim.SetTrigger("UpAttack");
+            }
+            else
+            {
+                anim.SetTrigger("DownAttack");
+            }
+        }
+        else
+        {
+            if (lastMoveDirection.x > 0f)
+            {
+                anim.SetTrigger("RightAttack");
+            }
+            else
+            {
+                anim.SetTrigger("LeftAttack");
+            }
+        }
+
+        // Detect enemies in attack range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position + (Vector3)lastMoveDirection * attackRange, attackRange);
 
         foreach (Collider2D enemy in hitEnemies)
         {
             if (enemy.CompareTag("Enemy"))
             {
-                Debug.Log("Found an enemy with the 'Enemy' tag.");
                 EnemyHealth enemyHealth = enemy.GetComponent<EnemyHealth>();
                 if (enemyHealth != null)
                 {
                     enemyHealth.TakeDamage(damage);
-                    Debug.Log("Enemy attacked!");
                 }
-                else
-                {
-                    Debug.Log("Enemy does not have an EnemyHealth component.");
-                }
-            }
-            else
-            {
-                Debug.Log("Found object, but it does not have the 'Enemy' tag.");
             }
         }
+    }
+
+    // Animation event functions to reset the attack flag
+    public void AttackAnimationComplete()
+    {
+        isAttacking = false;
     }
 
 
     private void OnDrawGizmosSelected()
     {
-        // Draw the attack range when the object is selected in the scene
+        // Draw the attack range when the object is selected in the sceneS
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.DrawWireSphere(transform.position + (Vector3)playerMovement.lastMoveDirection * attackRange, attackRange);
     }
-
 }
--- DemoBossRushGame/Assets/Script/Player Script/PlayerMovement.cs
+++ DemoBossRushGame/Assets/Script/Player Script/PlayerMovement.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,8 +14,7 @@ public class PlayerMovement : MonoBehaviour
     public float dodgeDuration = 0.2f;
 
     private bool isDodging = false;
-    public bool facingRight = true;
-    private Vector2 lastMove = Vector2.down; // Last Move Direction // Default direction is Down Animation //
+    public Vector2 lastMoveDirection = Vector2.down; // Default direction is Down
 
     void Start()
     {
@@ -26,94 +23,76 @@ public class PlayerMovement : MonoBehaviour
 
         var playerActions = inputActions.FindActionMap("Player");
         playerActions.FindAction("Move").performed += OnMove;
-        playerActions.FindAction("Move").canceled += OnMove; // Cancel the movement when the button is released
+        playerActions.FindAction("Move").canceled += OnMove;
         playerActions.FindAction("Dodge").performed += OnDodge;
 
-        playerActions.Enable(); // Activate the action map
+        playerActions.Enable();
     }
 
     void FixedUpdate()
     {
-        if (!isDodging)
+        if (!isDodging && moveInput != Vector2.zero)
         {
-            if (moveInput != Vector2.zero)
-            {
-                Vector2 move = moveInput * moveSpeed * Time.deltaTime; // Calculate the input
-                rb.MovePosition(rb.position + move); // Move the player
-            }
+            Vector2 move = moveInput * moveSpeed * Time.deltaTime;
+            rb.MovePosition(rb.position + move);
         }
     }
 
     void Update()
     {
-        // Animation Parameter
+        // Set animator parameters
         anim.SetFloat("Horizontal", moveInput.x);
         anim.SetFloat("Vertical", moveInput.y);
         anim.SetFloat("Speed", moveInput.sqrMagnitude);
 
-        if(moveInput.sqrMagnitude > 0.01f) // Player is moving
+        // Check if the player is moving
+        if (moveInput.sqrMagnitude > 0.01f)
         {
-            lastMove = moveInput; // Update the last move direction
+            // Update the last move direction
+            lastMoveDirection = moveInput;
         }
         else
         {
-            if(Mathf.Abs(lastMove.y) > MathF.Abs(lastMove.x))
+            // Handle directional idle based on the last move direction
+            if (Mathf.Abs(lastMoveDirection.y) > Mathf.Abs(lastMoveDirection.x))
             {
-                if(lastMove.y > 0)
-                {
+                // Vertical direction takes precedence
+                if (lastMoveDirection.y > 0)
                     anim.Play("Up Idle");
-                }
                 else
-                {
                     anim.Play("Down Idle");
-                }
             }
             else
             {
-                if (lastMove.x > 0)
-                {
+                // Horizontal direction
+                if (lastMoveDirection.x > 0)
                     anim.Play("Right Idle");
-                }
                 else
-                {
                     anim.Play("Left Idle");
-                }
             }
         }
-        }
-
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>(); // Read the movement input
+        moveInput = context.ReadValue<Vector2>();
     }
 
     public void OnDodge(InputAction.CallbackContext context)
     {
         if (context.performed && !isDodging)
         {
-            StartCoroutine(Dodge()); // Dodge
+            StartCoroutine(Dodge());
         }
     }
 
     private IEnumerator Dodge()
     {
         isDodging = true;
-        rb.velocity = moveInput.normalized * dodgeSpeed;
+        Vector2 dodgeDirection = moveInput != Vector2.zero ? moveInput.normalized : lastMoveDirection;
+        rb.velocity = dodgeDirection * dodgeSpeed;
         yield return new WaitForSeconds(dodgeDuration);
         rb.velocity = Vector2.zero;
         isDodging = false;
     }
-
-   /* private void Flip()
-    {
-        if ((moveInput.x < 0 && facingRight) || (moveInput.x > 0 && !facingRight))
-        {
-            facingRight = !facingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f; // Flip the sprite
-            transform.localScale = localScale;
-        }
-    }*/
 }
